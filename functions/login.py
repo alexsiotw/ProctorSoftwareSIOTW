5cc64993df5fce670177d7bd43b7143c3568d643
alexsiotw <alex@siotw.net>
2025-09-17 17:13:27 -0400

    Update login.py

--- functions/login.py
+++ functions/login.py
@@ -1,24 +1,23 @@
 import json
-from flask import request
-from pusher import Pusher
-import os
-
-pusher = Pusher(
-    app_id=os.environ['PUSHER_APP_ID'],
-    key=os.environ['PUSHER_KEY'],
-    secret=os.environ['PUSHER_SECRET'],
-    cluster=os.environ['PUSHER_CLUSTER'],
-    ssl=True
-)
+from http import HTTPStatus
 
 TEACHER_USER = 'admin'
 TEACHER_PASS = 'password'
 
 def handler(event, context):
     try:
-        body = json.loads(event['body'])
-        if body['username'] == TEACHER_USER and body['password'] == TEACHER_PASS:
-            return {'statusCode': 200, 'body': json.dumps({'success': True, 'is_teacher': True})}
-        return {'statusCode': 200, 'body': json.dumps({'success': False})}
+        body = json.loads(event.get('body', '{}'))
+        if body.get('username') == TEACHER_USER and body.get('password') == TEACHER_PASS:
+            return {
+                'statusCode': HTTPStatus.OK,
+                'body': json.dumps({'success': True, 'is_teacher': True})
+            }
+        return {
+            'statusCode': HTTPStatus.OK,
+            'body': json.dumps({'success': False})
+        }
     except Exception as e:
-        return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
+        return {
+            'statusCode': HTTPStatus.INTERNAL_SERVER_ERROR,
+            'body': json.dumps({'error': str(e)})
+        }
